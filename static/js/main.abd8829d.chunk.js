c519bf16c57d86d10aed859cbc171531dc7c9ec4
GromovVv <pavel.kuts@gmail.com>
2021-05-05 04:48:00 +0300

    Updates

---
+++ static/js/main.abd8829d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{10:function(e,t,n){},11:function(e,t,n){"use strict";n.r(t);var c=n(1),i=n.n(c),r=n(3),a=n.n(r),o=(n(8),n(0));var s=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"Hallo"})})};n(10);a.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.abd8829d.chunk.js.map
